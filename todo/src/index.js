856a300a11b7f1de185557ab6b3de8f1b35a71d3
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2020-04-19 21:21:27 +0400

    1

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello world');
+import React from "react";
+import ReactDom from "react-dom";
+
+const TodoList = () => {
+    const items = ['Learn React', 'Build Awesome App'];
+    return (
+      <ul>
+          <li>{items[0]}</li>
+          <li>{items[1]}</li>
+      </ul>
+  )
+};
+const AppHeader = () => {
+    return (
+        <h1>My Todo List</h1>
+    )
+};
+const SearchPanel = () => {
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return <input
+        style={searchStyle}
+        placeholder={searchText}/>
+};
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList />
+        </div>
+    )
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));
