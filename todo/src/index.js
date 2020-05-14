b855411e4fcf1fcd2931e21203a4e3aafa588b56
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2020-05-14 18:32:37 +0400

    continue with Todo

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,35 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
-const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App'];
-    return (
-      <ul>
-          <li>{items[0]}</li>
-          <li>{items[1]}</li>
-      </ul>
-  )
-};
-const AppHeader = () => {
-    return (
-        <h1>My Todo List</h1>
-    )
-};
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    return <input
-        style={searchStyle}
-        placeholder={searchText}/>
-};
 const App = () => {
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Make Awesome App', important: true, id: 2},
+        {label: 'Have a lunch', important: false, id: 3}
+    ];
     return (
         <div>
             <AppHeader/>
             <SearchPanel/>
-            <TodoList />
+            <TodoList todos={todoData}/>
         </div>
     )
 };
